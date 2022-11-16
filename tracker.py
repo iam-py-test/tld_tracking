2d2f5f004239d681b47a85d226b26c356eef7801
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-11-16 18:53:07 -0500

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,21 @@
+import os,sys
+import requests
+from publicsuffixlist import PublicSuffixList
+
+psl = PublicSuffixList()
+
+fulldomainslist = requests.get("https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt").text.split("\n")
+tlds = {}
+
+for domain in domains:
+  try:
+    tld = psl.publicsuffix(domain)
+    if tld not in tlds:
+      tlds[tld] = 0
+    tlds[tld] += 1
+  except:
+    pass
+outlist = open("output.md",'w')
+for tld in tlds:
+  outlist.write("{}: {}\n".format(tld,tlds[tld]))
+outlist.close()
