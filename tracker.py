df698ece6a9c36c59078522ef1f3747361cc7acd
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-12-29 11:20:44 -0500

    Include more than one list

--- tracker.py
+++ tracker.py
@@ -4,21 +4,37 @@ from publicsuffixlist import PublicSuffixList
 
 psl = PublicSuffixList()
 
-domains = requests.get("https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt").text.split("\n")
-tlds = {}
+lists = {"The malicious website blocklist":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt","iam-py-test's anti-PUP list":"https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antipup_domains.txt"}
+lists_data = {}
+tldsg = {}
 
-for domain in domains:
-  try:
-    tld = psl.publicsuffix(domain)
-    if tld == None:
-      print(domain)
-      continue
-    if tld not in tlds:
-      tlds[tld] = 0
-    tlds[tld] += 1
-  except:
-    pass
+
+tlds = {}
+for l in lists:
+  domains = requests.get(lists[l]).text.split("\n")
+  for domain in domains:
+    try:
+      tld = psl.publicsuffix(domain)
+      if tld == None:
+        print(domain)
+        continue
+      if tld not in tlds:
+        tlds[tld] = 0
+      tlds[tld] += 1
+      if tld not in tldsg:
+        tldsg[tld] = 0
+      tldsg[tld] += 1
+    except:
+      pass
+  lists_data[l] = tlds
 outlist = open("output.md",'w')
-for tld in tlds:
-  outlist.write("{}: {}<br>\n".format(tld,tlds[tld]))
+outlist.write("## All lists\n")
+for tld in tldsg:
+  outlist.write("{}: {}<br>\n".format(tld,tldsg[tld]))
+
+for ldata in lists_data:
+  outlist.write("## {}\n".format(ldata))
+  ltlds = lists_data[ldata]
+  for tld in ltlds:
+    outlist.write("{}: {}<br>\n".format(tld,ltlds[tld]))
 outlist.close()
